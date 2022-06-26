26c7b7252afb9eb5dd932a13c111ea3c9d133982
graziellecafe <65823579+graziellecafe@users.noreply.github.com>
2022-06-26 18:07:01 -0300

    Create the project and setup some css

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Buscador.module.scss"; 
+
+interface Props {
+    busca: string, 
+    setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Buscador({ busca, setBusca }: Props) {
+    return(
+        <div className={styles.buscador}>
+            <input
+                value={busca}
+                onChange={(evento) => setBusca(evento.target.value)}                
+            />
+        </div>
+    )
+}
