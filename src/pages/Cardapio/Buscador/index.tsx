aa6b2b066b38fa15b781aaccdde217630958ef1a
graziellecafe <65823579+graziellecafe@users.noreply.github.com>
2022-06-27 11:34:00 -0300

    Developing ordenation list

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Buscador.module.scss"; 
+import { CgSearch } from "react-icons/cg"
 
 interface Props {
     busca: string, 
@@ -11,7 +12,12 @@ export default function Buscador({ busca, setBusca }: Props) {
         <div className={styles.buscador}>
             <input
                 value={busca}
-                onChange={(evento) => setBusca(evento.target.value)}                
+                onChange={(evento) => setBusca(evento.target.value)} 
+                placeholder="Buscar"               
+            />
+            <CgSearch
+                size={20}
+                color="#4c4d5e"
             />
         </div>
     )
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import styles from "./Cardapio.module.scss"
 import { ReactComponent as Logo } from "assets/logo.svg"
 import Buscador from "./Buscador"
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
+
     return (
         <main>
             <nav className={styles.menu}>
@@ -20,6 +25,10 @@ export default function Cardapio() {
                 <Buscador 
                     busca={busca} 
                     setBusca={setBusca}/>
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
+                </div>
             </section>
         </main>
     )
